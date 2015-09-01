2d0887e0916d951a593cc21c584e3aa7fd93833d
vsantos10 <vnrs90@gmail.com>
2015-09-01 18:12:35 +0100

    v1
    Login form e Registo

--- assets/global/plugins/jquery-validation/js/jquery.validate.js
+++ assets/global/plugins/jquery-validation/js/jquery.validate.js
@@ -324,10 +324,10 @@ $.extend( $.validator, {
 	},
 
 	messages: {
-		required: "This field is required.",
-		remote: "Please fix this field.",
-		email: "Please enter a valid email address.",
-		url: "Please enter a valid URL.",
+		required: "Preenchimento obrigatório.",
+		remote: "Corrija este campo.",
+		email: "Introduza um e-mail válido.",
+		url: "Introduza um url válido.",
 		date: "Please enter a valid date.",
 		dateISO: "Please enter a valid date ( ISO ).",
 		number: "Please enter a valid number.",
