77b8d523a994941bd50b4f3f0e7e03b5f4d282a0
vsantos10 <vnrs90@gmail.com>
2015-10-20 16:19:29 +0100

    V6.7

---
+++ notadeentrega.php
@@ -0,0 +1,464 @@
+<!DOCTYPE html>
+<!--
+
+
+Author:
+Website: http://www..com/
+Contact: support@.com
+Follow: www.twitter.com/
+
+
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+<meta charset="utf-8"/>
+<title>Declaração de Fornecimento</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta content="" name="description"/>
+<meta content="" name="author"/>
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css">
+<link href="assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css">
+<link href="assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css">
+<!-- END GLOBAL MANDATORY STYLES -->
+<!-- BEGIN PAGE LEVEL STYLES -->
+<link rel="stylesheet" type="text/css" href="assets/global/plugins/select2/select2.css"/>
+<!-- END PAGE LEVEL SCRIPTS -->
+<!-- BEGIN THEME STYLES -->
+<link href="assets/global/css/components-rounded.css" id="style_components" rel="stylesheet" type="text/css">
+<link href="assets/global/css/plugins.css" rel="stylesheet" type="text/css">
+<link href="assets/admin/layout3/css/layout.css" rel="stylesheet" type="text/css">
+<link href="assets/admin/layout3/css/themes/default.css" rel="stylesheet" type="text/css" id="style_color">
+<link href="assets/admin/layout3/css/custom.css" rel="stylesheet" type="text/css">
+<!-- END THEME STYLES -->
+<link rel="shortcut icon" href="favicon.ico"/>
+</head>
+<!-- END HEAD -->
+<!-- BEGIN BODY -->
+<!-- DOC: Apply "page-header-menu-fixed" class to set the mega menu fixed  -->
+<!-- DOC: Apply "page-header-top-fixed" class to set the top menu fixed  -->
+<body>
+<!-- BEGIN HEADER -->
+<div class="page-header">
+	<!-- BEGIN HEADER TOP -->
+		<?php include 'header.html'; ?>
+	<!-- END HEADER TOP -->
+	<!-- BEGIN HEADER MENU -->
+	<div class="page-header-menu">
+		<div class="container">
+			<!-- BEGIN HEADER SEARCH BOX -->
+			<form class="search-form" action="extra_search.html" method="GET">
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Procurar" name="query">
+					<span class="input-group-btn">
+					<a href="javascript:;" class="btn submit"><i class="icon-magnifier"></i></a>
+					</span>
+				</div>
+			</form>
+			<!-- END HEADER SEARCH BOX -->
+			<!-- BEGIN MEGA MENU -->
+			<!-- DOC: Apply "hor-menu-light" class after the "hor-menu" class below to have a horizontal menu with white background -->
+			<!-- DOC: Remove data-hover="dropdown" and data-close-others="true" attributes below to disable the dropdown opening on mouse hover -->
+		<?php include 'menu.html'; ?>
+			<!-- END MEGA MENU -->
+		</div>
+	</div>
+	<!-- END HEADER MENU -->
+</div>
+<!-- END HEADER -->
+<!-- BEGIN PAGE CONTAINER -->
+<div class="page-container">
+	<!-- BEGIN PAGE HEAD -->
+	<div class="page-head">
+		<div class="container">
+			<!-- BEGIN PAGE TITLE -->
+			<div class="page-title">
+
+			</div>
+			<!-- END PAGE TITLE -->
+			<!-- BEGIN PAGE TOOLBAR -->
+
+				<!-- END THEME PANEL -->
+			</div>
+			<!-- END PAGE TOOLBAR -->
+		</div>
+	</div>
+	<!-- END PAGE HEAD -->
+	<!-- BEGIN PAGE CONTENT -->
+	<div class="page-content">
+		<div class="container">
+			<!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+			<div class="modal fade" id="portlet-config" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+							<h4 class="modal-title">Modal title</h4>
+						</div>
+						<div class="modal-body">
+							 Widget settings form goes here
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn blue">Save changes</button>
+							<button type="button" class="btn default" data-dismiss="modal">Close</button>
+						</div>
+					</div>
+					<!-- /.modal-content -->
+				</div>
+				<!-- /.modal-dialog -->
+			</div>
+			<!-- /.modal -->
+			<!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+			<!-- BEGIN PAGE CONTENT INNER -->
+			<div class="row">
+				<div class="col-md-12">
+					<div class="tabbable tabbable-custom tabbable-noborder tabbable-reversed">
+						<ul class="nav nav-tabs">
+							<li  class="active">
+								<a  href="#tab_2" data-toggle="tab">
+							 </a>
+							</li>
+						</ul>
+
+										<!-- END FORM-->
+									</div>
+								</div>
+								<div class="portlet light bordered">
+									<div class="portlet-title">
+										<div class="caption">
+
+											<span class="caption-subject font-blue-sunglo bold uppercase">	Nota de Entrega</span>
+
+										</div>
+
+									</div>
+									<div class="portlet-body form">
+										<!-- BEGIN FORM-->
+										<form action="javascript:;" class="form-horizontal">
+											<div class="form-body">
+
+												<div class="row">
+													<div class="col-md-6">
+														<div class="form-group">
+															<label class="control-label col-md-3">Data</label>
+															<div class="col-md-9">
+																<input type="text" class="form-control" >
+															</div>
+														</div>
+													</div>
+
+													<!--/span-->
+												</div>
+												<div class="row">
+												<div class="col-md-9">
+													<div class="form-group">
+														<label class="control-label col-md-2">Ex.mo(s) Sr.(s)</label>
+														<div class="col-md-10">
+														<input type="text" class="form-control" >
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="row">
+											<div class="col-md-9">
+												<div class="form-group">
+													<label class="control-label col-md-2">Morada</label>
+													<div class="col-md-10">
+													<input type="text" class="form-control" >
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group">
+													<label class="control-label col-md-3">NIPC</label>
+													<div class="col-md-9">
+														<input type="text" class="form-control" >
+													</div>
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="form-group">
+													<label class="control-label col-md-3">Contrato Nº</label>
+													<div class="col-md-9">
+														<input type="text" class="form-control" >
+													</div>
+												</div>
+											</div>
+										</div>
+											<h3 class="form-section">Rolaria FSC 100%</h3>
+											<br/>
+											<h4 class="form-section">Proveniência</h3>
+											<!--/span-->
+										</div>
+												<!--/row-->
+												<div class="row">
+													<div class="col-md-6">
+														<div class="form-group">
+															<label class="control-label col-md-3">Código da Mata</label>
+															<div class="col-md-9">
+																<input type="text" class="form-control" >
+															</div>
+														</div>
+													</div>
+													<div class="col-md-6">
+														<div class="form-group">
+															<label class="control-label col-md-3">Povoamento Nº</label>
+															<div class="col-md-9">
+																<input type="text" class="form-control" >
+															</div>
+														</div>
+													</div>
+												</div>
+												<div class="row">
+																								<div class="col-md-9">
+																									<div class="form-group">
+																										<label class="control-label col-md-3">Espécie</label>
+
+																										<div class="col-md-9">
+																											<div class="radio-list">
+																												<label class="radio-inline">
+																												<input type="radio" name="optionsRadios2" value="option1"/>
+																												Eucalipto</label>
+																												<label class="radio-inline">
+																												<input type="radio" name="optionsRadios2" value="option2" checked/>
+																												Pinho</label>
+
+																												<label class="radio-inline">
+																												<input type="radio" name="optionsRadios2" value="option2" checked/>Outra
+																												<input type="text" class="form-control" >
+																												</label>
+																											</div>
+																										</div>
+																									</div>
+																								</div>
+																							</div>
+																							<div class="row">
+																								<div class="col-md-6">
+																									<div class="form-group">
+																										<label class="control-label col-md-3">Quantidade</label>
+																										<div class="col-md-8">
+																											<input type="text" class="form-control" >M3 / TN
+																										</div>
+																									</div>
+																								</div>
+																								<div class="col-md-6">
+																									<div class="form-group">
+																										<label class="control-label col-md-3">Preço Unitário</label>
+																										<div class="col-md-8">
+																											<input type="text" class="form-control" > €
+																										</div>
+																									</div>
+																								</div>
+																							</div>
+																							<div class="row">
+																							<div class="col-md-9">
+																								<div class="form-group">
+																									<label class="control-label col-md-2">Localidade</label>
+																									<div class="col-md-10">
+																									<input type="text" class="form-control" >
+																									</div>
+																								</div>
+																							</div>
+																						</div>
+																						<div class="row">
+																						<div class="col-md-9">
+																							<div class="form-group">
+																								<label class="control-label col-md-2">Freguesia</label>
+																								<div class="col-md-10">
+																								<input type="text" class="form-control" >
+																								</div>
+																							</div>
+																						</div>
+																					</div>
+																					<div class="row">
+																					<div class="col-md-9">
+																						<div class="form-group">
+																							<label class="control-label col-md-2">Concelho</label>
+																							<div class="col-md-10">
+																							<input type="text" class="form-control" >
+																							</div>
+																						</div>
+																					</div>
+																				</div>
+																					<h4 class="form-section">Outras Referências</h3>
+
+													<div class="row">
+													<div class="col-md-9">
+														<div class="form-group">
+															<label class="control-label col-md-2">Subcodigo de Membro GGFA</label>
+															<div class="col-md-10">
+															<input type="text" class="form-control" >
+															</div>
+														</div>
+													</div>
+												</div>
+												<div class="row">
+												<div class="col-md-9">
+													<div class="form-group">
+														<label class="control-label col-md-2">Subcodigo de Membro BFA</label>
+														<div class="col-md-10">
+														<input type="text" class="form-control" >
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="row">
+											<div class="col-md-9">
+												<div class="form-group">
+													<label class="control-label col-md-2">Operador</label>
+													<div class="col-md-10">
+													<input type="text" class="form-control" >
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="row">
+										<div class="col-md-9">
+											<div class="form-group">
+												<label class="control-label col-md-2">Transportador:</label>
+												<div class="col-md-10">
+												<input type="text" class="form-control" >
+												</div>
+											</div>
+										</div>
+									</div>
+												<h3 class="form-section">Transporte</h3>
+
+												<div class="row">
+													<div class="col-md-6">
+														<div class="form-group">
+															<label class="control-label col-md-3">Data da carga</label>
+															<div class="col-md-9">
+																<input type="text" class="form-control" >
+															</div>
+														</div>
+													</div>
+												</div>
+													<div class="row">
+														<div class="col-md-6">
+															<div class="form-group">
+																<label class="control-label col-md-3">Hora da Carga</label>
+																<div class="col-md-9">
+																	<input type="text" class="form-control" >
+																</div>
+															</div>
+														</div>
+														<div class="col-md-6">
+															<div class="form-group">
+																<label class="control-label col-md-3">Hora da Descarga</label>
+																<div class="col-md-9">
+																	<input type="text" class="form-control" >
+																</div>
+															</div>
+														</div>
+														<div class="row">
+														<div class="col-md-9">
+															<div class="form-group">
+																<label class="control-label col-md-2">Local da Descarga</label>
+																<div class="col-md-10">
+																<input type="text" class="form-control" >
+																</div>
+															</div>
+														</div>
+													</div>
+													<div class="row">
+														<div class="col-md-6">
+															<div class="form-group">
+																<label class="control-label col-md-3">Matrícula Viatura</label>
+																<div class="col-md-9">
+																	<input type="text" class="form-control" >
+																</div>
+															</div>
+														</div>
+														<div class="col-md-6">
+															<div class="form-group">
+																<label class="control-label col-md-3">Reboque</label>
+																<div class="col-md-9">
+																	<input type="text" class="form-control" >
+																</div>
+															</div>
+														</div>
+													</div>
+
+
+														<!--/span-->
+													</div>
+
+													<!--/span-->
+												</div>
+
+												<!--/row-->
+
+										</form>
+										<!-- END FORM-->
+									</div>
+								</div>
+
+										<!-- END FORM-->
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- END PAGE CONTENT INNER -->
+		</div>
+	</div>
+	<!-- END PAGE CONTENT -->
+</div>
+<!-- END PAGE CONTAINER -->
+<!-- BEGIN PRE-FOOTER -->
+<?php include 'footer.html'; ?>
+<!-- END FOOTER -->
+<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+<!-- BEGIN CORE PLUGINS -->
+<!--[if lt IE 9]>
+<script src="assets/global/plugins/respond.min.js"></script>
+<script src="assets/global/plugins/excanvas.min.js"></script>
+<![endif]-->
+<script src="assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+<!-- IMPORTANT! Load jquery-ui.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
+<script src="assets/global/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+<script src="assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+<script src="assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="assets/global/plugins/jquery.cokie.min.js" type="text/javascript"></script>
+<script src="assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script type="text/javascript" src="assets/global/plugins/select2/select2.min.js"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="assets/global/scripts/metronic.js" type="text/javascript"></script>
+<script src="assets/admin/layout3/scripts/layout.js" type="text/javascript"></script>
+<script src="assets/admin/layout3/scripts/demo.js" type="text/javascript"></script>
+<script src="assets/admin/pages/scripts/form-samples.js"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+<script>
+jQuery(document).ready(function() {
+   // initiate layout and plugins
+   Metronic.init(); // init metronic core components
+Layout.init(); // init current layout
+Demo.init(); // init demo features
+   FormSamples.init();
+});
+</script>
+<!-- END JAVASCRIPTS -->
+</body>
+<!-- END BODY -->
+</html>
